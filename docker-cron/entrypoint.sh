14fd31fa79e1f997450718c866f64ce5c4178eaf
Andrew Martin <andy123@stanford.edu>
2018-10-18 17:23:00 -0700

    Adding better cron

--- docker-cron/entrypoint.sh
+++ docker-cron/entrypoint.sh
@@ -1,13 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-# command: ['crond', '-f', '/var/spool/cron/crontabs']
-#set -e
-#
-#if [ ! -z "$MH_MAILDIR_PATH" ]; then
-#  echo "Verifying permissions of $MH_MAILDIR_PATH"
-#  mkdir -p $MH_MAILDIR_PATH
-#  chown -R 1000 $MH_MAILDIR_PATH
-#fi
+set -e
 
+if [ ! -z "$APACHE_RUN_USER_ID" ]; then
+    if [ $(id -u $APACHE_RUN_USER_ID >/dev/null 2>&1) ]; then
+        echo "User ${APACHE_RUN_USER_ID} already exists"
+    else
+        echo "Adding user ${APACHE_RUN_USER_ID}"
+        adduser -D -H -u "$APACHE_RUN_USER_ID" www-data
+    fi
+fi
 
-exec crond -f /var/spool/cron/crontabs
+# Log Rotate throws an error if this file doesn't exist
+touch /var/log/messages
+
+# Start up the cronjobs
+/usr/sbin/crond -f /var/spool/cron/crontabs
