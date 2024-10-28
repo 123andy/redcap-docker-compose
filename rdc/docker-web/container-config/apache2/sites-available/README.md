83f8e4c578819c0fd53ff5d8093b0c827f2d1f5e
Andy Martin <andy123@stanford.edu>
2024-10-28 11:18:09 -0700

    Updating cron, logging, and base docker image setup

--- rdc/docker-web/container-config/apache2/sites-available/README.md
+++
@@ -1,2 +0,0 @@
-You can add configuration info to the default virtualhost 000-site.conf by using the site.custom file.
-All other .conf files in the sites-enabled directory will be loaded.
--- rdc/docker-web/container-config/startup-scripts/50-remove_default_vhost.sh
+++ rdc/docker-web/container-config/startup-scripts/50-remove_default_vhost.sh
@@ -5,4 +5,9 @@
 if [[ "${REMOVE_DEFAULT_VHOST}" = true ]]; then
     rm /etc/apache2/sites-enabled/000-default.conf
     echo "Removed 000-default site"
+else
+    # unless we are removing the default vhost, lets turn off the other vhost logging
+    echo "Remove other-vhosts-access-log"
+    #rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf
+    a2disconf other-vhosts-access-log
 fi
